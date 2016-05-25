20c0275ddd9b9f0805468b752af455ed195aecf7
paramat <mat.gregory@virginmedia.com>
2016-05-25 07:59:47 +0100

    Init.lua: Organise, improve code style

--- init.lua
+++ init.lua
@@ -1,172 +1,69 @@
--- Minetest 0.4 Mod: Nether
+-- Parameters
 
 local NETHER_DEPTH = -5000
 
-minetest.register_node("nether:portal", {
-	description = "Nether Portal",
-	tiles = {
-		"nether_transparent.png",
-		"nether_transparent.png",
-		"nether_transparent.png",
-		"nether_transparent.png",
-		{
-			name = "nether_portal.png",
-			animation = {
-				type = "vertical_frames",
-				aspect_w = 16,
-				aspect_h = 16,
-				length = 0.5,
-			},
-		},
-		{
-			name = "nether_portal.png",
-			animation = {
-				type = "vertical_frames",
-				aspect_w = 16,
-				aspect_h = 16,
-				length = 0.5,
-			},
-		},
-	},
-	drawtype = "nodebox",
-	paramtype = "light",
-	paramtype2 = "facedir",
-	sunlight_propagates = true,
-	use_texture_alpha = true,
-	walkable = false,
-	digable = false,
-	pointable = false,
-	buildable_to = false,
-	drop = "",
-	light_source = 5,
-	post_effect_color = {a=180, r=128, g=0, b=128},
-	alpha = 192,
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.5, -0.5, -0.1,  0.5, 0.5, 0.1},
-		},
-	},
-	groups = {not_in_creative_inventory=1}
-})
+
+-- Functions
 
 local function build_portal(pos, target)
-	local p = {x=pos.x-1, y=pos.y-1, z=pos.z}
-	local p1 = {x=pos.x-1, y=pos.y-1, z=pos.z}
-	local p2 = {x=p1.x+3, y=p1.y+4, z=p1.z}
-	for i=1,4 do
-		minetest.set_node(p, {name="default:obsidian"})
-		p.y = p.y+1
+	local p = {x = pos.x - 1, y = pos.y - 1, z = pos.z}
+	local p1 = {x = pos.x - 1, y = pos.y - 1, z = pos.z}
+	local p2 = {x = p1.x + 3, y = p1.y + 4, z = p1.z}
+
+	for i = 1, 4 do
+		minetest.set_node(p, {name = "default:obsidian"})
+		p.y = p.y + 1
 	end
-	for i=1,3 do
-		minetest.set_node(p, {name="default:obsidian"})
-		p.x = p.x+1
+	for i = 1, 3 do
+		minetest.set_node(p, {name = "default:obsidian"})
+		p.x = p.x + 1
 	end
-	for i=1,4 do
-		minetest.set_node(p, {name="default:obsidian"})
-		p.y = p.y-1
+	for i = 1, 4 do
+		minetest.set_node(p, {name = "default:obsidian"})
+		p.y = p.y - 1
 	end
-	for i=1,3 do
-		minetest.set_node(p, {name="default:obsidian"})
-		p.x = p.x-1
+	for i = 1, 3 do
+		minetest.set_node(p, {name = "default:obsidian"})
+		p.x = p.x - 1
 	end
-	for x=p1.x,p2.x do
-	for y=p1.y,p2.y do
-		p = {x=x, y=y, z=p1.z}
-		if not (x == p1.x or x == p2.x or y==p1.y or y==p2.y) then
-			minetest.set_node(p, {name="nether:portal", param2=0})
+
+	for x = p1.x, p2.x do
+	for y = p1.y, p2.y do
+		p = {x = x, y = y, z = p1.z}
+		if not (x == p1.x or x == p2.x or y == p1.y or y == p2.y) then
+			minetest.set_node(p, {name = "nether:portal", param2 = 0})
 		end
 		local meta = minetest.get_meta(p)
 		meta:set_string("p1", minetest.pos_to_string(p1))
 		meta:set_string("p2", minetest.pos_to_string(p2))
 		meta:set_string("target", minetest.pos_to_string(target))
-		
+
 		if y ~= p1.y then
-			for z=-2,2 do
+			for z = -2, 2 do
 				if z ~= 0 then
-					p.z = p.z+z
+					p.z = p.z + z
 					if minetest.registered_nodes[minetest.get_node(p).name].is_ground_content then
 						minetest.remove_node(p)
 					end
-					p.z = p.z-z
+					p.z = p.z - z
 				end
 			end
 		end
-		
 	end
 	end
 end
 
-minetest.register_abm({
-	nodenames = {"nether:portal"},
-	interval = 1,
-	chance = 2,
-	action = function(pos, node)
-		minetest.add_particlespawner(
-			32, --amount
-			4, --time
-			{x=pos.x-0.25, y=pos.y-0.25, z=pos.z-0.25}, --minpos
-			{x=pos.x+0.25, y=pos.y+0.25, z=pos.z+0.25}, --maxpos
-			{x=-0.8, y=-0.8, z=-0.8}, --minvel
-			{x=0.8, y=0.8, z=0.8}, --maxvel
-			{x=0,y=0,z=0}, --minacc
-			{x=0,y=0,z=0}, --maxacc
-			0.5, --minexptime
-			1, --maxexptime
-			1, --minsize
-			2, --maxsize
-			false, --collisiondetection
-			"nether_particle.png" --texture
-		)
-		for _,obj in ipairs(minetest.get_objects_inside_radius(pos, 1)) do
-			if obj:is_player() then
-				local meta = minetest.get_meta(pos)
-				local target = minetest.string_to_pos(meta:get_string("target"))
-				if target then
-					-- force emerge of target area
-					minetest.get_voxel_manip():read_from_map(target, target)
-					if not minetest.get_node_or_nil(target) then
-						minetest.emerge_area(vector.subtract(target, 80), vector.add(target, 80))
-					end
-					-- teleport the player
-					minetest.after(3, function(obj, pos, target)
-						local objpos = obj:getpos()
-						objpos.y = objpos.y+0.1 -- Fix some glitches at -8000
-						if minetest.get_node(objpos).name ~= "nether:portal" then
-							return
-						end
-						
-						obj:setpos(target)
-						
-						local function check_and_build_portal(pos, target)
-							local n = minetest.get_node_or_nil(target)
-							if n and n.name ~= "nether:portal" then
-								build_portal(target, pos)
-								minetest.after(2, check_and_build_portal, pos, target)
-								minetest.after(4, check_and_build_portal, pos, target)
-							elseif not n then
-								minetest.after(1, check_and_build_portal, pos, target)
-							end
-						end
-						
-						minetest.after(1, check_and_build_portal, pos, target)
-						
-					end, obj, pos, target)
-				end
-			end
-		end
-	end,
-})
-
 local function move_check(p1, max, dir)
-	local p = {x=p1.x, y=p1.y, z=p1.z}
-	local d = math.abs(max-p1[dir]) / (max-p1[dir])
+	local p = {x = p1.x, y = p1.y, z = p1.z}
+	local d = math.abs(max - p1[dir]) / (max - p1[dir])
+
 	while p[dir] ~= max do
 		p[dir] = p[dir] + d
 		if minetest.get_node(p).name ~= "default:obsidian" then
 			return false
 		end
 	end
+
 	return true
 end
 
@@ -200,15 +97,16 @@ local function check_portal(p1, p2)
 end
 
 local function is_portal(pos)
-	for d=-3,3 do
-		for y=-4,4 do
-			local px = {x=pos.x+d, y=pos.y+y, z=pos.z}
-			local pz = {x=pos.x, y=pos.y+y, z=pos.z+d}
-			if check_portal(px, {x=px.x+3, y=px.y+4, z=px.z}) then
-				return px, {x=px.x+3, y=px.y+4, z=px.z}
+	for d = -3, 3 do
+		for y = -4, 4 do
+			local px = {x = pos.x + d, y = pos.y + y, z = pos.z}
+			local pz = {x = pos.x, y = pos.y + y, z = pos.z + d}
+
+			if check_portal(px, {x = px.x + 3, y = px.y + 4, z = px.z}) then
+				return px, {x = px.x + 3, y = px.y + 4, z = px.z}
 			end
-			if check_portal(pz, {x=pz.x, y=pz.y+4, z=pz.z+3}) then
-				return pz, {x=pz.x, y=pz.y+4, z=pz.z+3}
+			if check_portal(pz, {x = pz.x, y = pz.y + 4, z = pz.z + 3}) then
+				return pz, {x = pz.x, y = pz.y + 4, z = pz.z + 3}
 			end
 		end
 	end
@@ -220,13 +118,13 @@ local function make_portal(pos)
 		return false
 	end
 	
-	for d=1,2 do
-	for y=p1.y+1,p2.y-1 do
+	for d = 1, 2 do
+	for y = p1.y + 1, p2.y - 1 do
 		local p
 		if p1.z == p2.z then
-			p = {x=p1.x+d, y=y, z=p1.z}
+			p = {x = p1.x + d, y = y, z = p1.z}
 		else
-			p = {x=p1.x, y=y, z=p1.z+d}
+			p = {x = p1.x, y = y, z = p1.z + d}
 		end
 		if minetest.get_node(p).name ~= "air" then
 			return false
@@ -235,9 +133,13 @@ local function make_portal(pos)
 	end
 	
 	local param2
-	if p1.z == p2.z then param2 = 0 else param2 = 1 end
+	if p1.z == p2.z then
+		param2 = 0
+	else
+		param2 = 1
+	end
 	
-	local target = {x=p1.x, y=p1.y, z=p1.z}
+	local target = {x = p1.x, y = p1.y, z = p1.z}
 	target.x = target.x + 1
 	if target.y < NETHER_DEPTH then
 		target.y = math.random(-50, 20)
@@ -245,12 +147,16 @@ local function make_portal(pos)
 		target.y = NETHER_DEPTH - math.random(500, 1500)
 	end
 	
-	for d=0,3 do
-	for y=p1.y,p2.y do
+	for d = 0, 3 do
+	for y = p1.y, p2.y do
 		local p = {}
-		if param2 == 0 then p = {x=p1.x+d, y=y, z=p1.z} else p = {x=p1.x, y=y, z=p1.z+d} end
+		if param2 == 0 then
+			p = {x = p1.x + d, y = y, z = p1.z}
+		else
+			p = {x = p1.x, y = y, z = p1.z + d}
+		end
 		if minetest.get_node(p).name == "air" then
-			minetest.set_node(p, {name="nether:portal", param2=param2})
+			minetest.set_node(p, {name = "nether:portal", param2 = param2})
 		end
 		local meta = minetest.get_meta(p)
 		meta:set_string("p1", minetest.pos_to_string(p1))
@@ -258,15 +164,132 @@ local function make_portal(pos)
 		meta:set_string("target", minetest.pos_to_string(target))
 	end
 	end
+
 	return true
 end
+
+
+-- ABMs
+
+minetest.register_abm({
+	nodenames = {"nether:portal"},
+	interval = 1,
+	chance = 2,
+	action = function(pos, node)
+		minetest.add_particlespawner(
+			32, --amount
+			4, --time
+			{x = pos.x - 0.25, y = pos.y - 0.25, z = pos.z - 0.25}, --minpos
+			{x = pos.x + 0.25, y = pos.y + 0.25, z = pos.z + 0.25}, --maxpos
+			{x = -0.8, y = -0.8, z = -0.8}, --minvel
+			{x = 0.8, y = 0.8, z = 0.8}, --maxvel
+			{x = 0, y = 0, z = 0}, --minacc
+			{x = 0, y = 0, z = 0}, --maxacc
+			0.5, --minexptime
+			1, --maxexptime
+			1, --minsize
+			2, --maxsize
+			false, --collisiondetection
+			"nether_particle.png" --texture
+		)
+		for _, obj in ipairs(minetest.get_objects_inside_radius(pos, 1)) do
+			if obj:is_player() then
+				local meta = minetest.get_meta(pos)
+				local target = minetest.string_to_pos(meta:get_string("target"))
+				if target then
+					-- force emerge of target area
+					minetest.get_voxel_manip():read_from_map(target, target)
+					if not minetest.get_node_or_nil(target) then
+						minetest.emerge_area(
+							vector.subtract(target, 80), vector.add(target, 80))
+					end
+					-- teleport the player
+					minetest.after(3, function(obj, pos, target)
+						local objpos = obj:getpos()
+						objpos.y = objpos.y + 0.1 -- Fix some glitches at -8000
+						if minetest.get_node(objpos).name ~= "nether:portal" then
+							return
+						end
+						
+						obj:setpos(target)
+						
+						local function check_and_build_portal(pos, target)
+							local n = minetest.get_node_or_nil(target)
+							if n and n.name ~= "nether:portal" then
+								build_portal(target, pos)
+								minetest.after(2, check_and_build_portal, pos, target)
+								minetest.after(4, check_and_build_portal, pos, target)
+							elseif not n then
+								minetest.after(1, check_and_build_portal, pos, target)
+							end
+						end
+						
+						minetest.after(1, check_and_build_portal, pos, target)
+						
+					end, obj, pos, target)
+				end
+			end
+		end
+	end,
+})
+
+
+-- Nodes
+
+minetest.register_node("nether:portal", {
+	description = "Nether Portal",
+	tiles = {
+		"nether_transparent.png",
+		"nether_transparent.png",
+		"nether_transparent.png",
+		"nether_transparent.png",
+		{
+			name = "nether_portal.png",
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 0.5,
+			},
+		},
+		{
+			name = "nether_portal.png",
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 0.5,
+			},
+		},
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = true,
+	use_texture_alpha = true,
+	walkable = false,
+	diggable = false,
+	pointable = false,
+	buildable_to = false,
+	drop = "",
+	light_source = 5,
+	post_effect_color = {a = 180, r = 128, g = 0, b = 128},
+	alpha = 192,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.1,  0.5, 0.5, 0.1},
+		},
+	},
+	groups = {not_in_creative_inventory = 1}
+})
 
 minetest.register_node(":default:obsidian", {
 	description = "Obsidian",
 	tiles = {"default_obsidian.png"},
 	is_ground_content = true,
 	sounds = default.node_sound_stone_defaults(),
-	groups = {cracky=1,level=2},
+	groups = {cracky = 1, level = 2},
 	
 	on_destruct = function(pos)
 		local meta = minetest.get_meta(pos)
@@ -276,15 +299,16 @@ minetest.register_node(":default:obsidian", {
 		if not p1 or not p2 then
 			return
 		end
-		for x=p1.x,p2.x do
-		for y=p1.y,p2.y do
-		for z=p1.z,p2.z do
-			local nn = minetest.get_node({x=x,y=y,z=z}).name
+
+		for x = p1.x, p2.x do
+		for y = p1.y, p2.y do
+		for z = p1.z, p2.z do
+			local nn = minetest.get_node({x = x, y = y, z = z}).name
 			if nn == "default:obsidian" or nn == "nether:portal" then
 				if nn == "nether:portal" then
-					minetest.remove_node({x=x,y=y,z=z})
+					minetest.remove_node({x = x, y = y, z = z})
 				end
-				local m = minetest.get_meta({x=x,y=y,z=z})
+				local m = minetest.get_meta({x = x, y = y, z = z})
 				m:set_string("p1", "")
 				m:set_string("p2", "")
 				m:set_string("target", "")
@@ -292,6 +316,7 @@ minetest.register_node(":default:obsidian", {
 		end
 		end
 		end
+
 		meta = minetest.get_meta(target)
 		if not meta then
 			return
@@ -301,15 +326,16 @@ minetest.register_node(":default:obsidian", {
 		if not p1 or not p2 then
 			return
 		end
-		for x=p1.x,p2.x do
-		for y=p1.y,p2.y do
-		for z=p1.z,p2.z do
-			local nn = minetest.get_node({x=x,y=y,z=z}).name
+
+		for x = p1.x, p2.x do
+		for y = p1.y, p2.y do
+		for z = p1.z, p2.z do
+			local nn = minetest.get_node({x = x, y = y, z = z}).name
 			if nn == "default:obsidian" or nn == "nether:portal" then
 				if nn == "nether:portal" then
-					minetest.remove_node({x=x,y=y,z=z})
+					minetest.remove_node({x = x, y = y, z = z})
 				end
-				local m = minetest.get_meta({x=x,y=y,z=z})
+				local m = minetest.get_meta({x = x, y = y, z = z})
 				m:set_string("p1", "")
 				m:set_string("p2", "")
 				m:set_string("target", "")
@@ -320,32 +346,17 @@ minetest.register_node(":default:obsidian", {
 	end,
 })
 
-minetest.register_craftitem(":default:mese_crystal_fragment", {
-	description = "Mese Crystal Fragment",
-	inventory_image = "default_mese_crystal_fragment.png",
-	on_place = function(stack,_, pt)
-		if pt.under and minetest.get_node(pt.under).name == "default:obsidian" then
-			local done = make_portal(pt.under)
-			if done and not minetest.setting_getbool("creative_mode") then
-				stack:take_item()
-			end
-		end
-		return stack
-	end,
-})
-
 minetest.register_node("nether:rack", {
 	description = "Netherrack",
 	tiles = {"nether_rack.png"},
 	is_ground_content = true,
 	drop = {
 		max_items = 1,
-		items = {{
-			rarity = 3,
-			items = {"nether:rack"},
-		}}
+		items = {
+			{rarity = 3, items = {"nether:rack"}},
+		}
 	},
-	groups = {cracky=3,level=2},
+	groups = {cracky = 3, level = 2},
 	sounds = default.node_sound_stone_defaults(),
 })
 
@@ -353,9 +364,9 @@ minetest.register_node("nether:sand", {
 	description = "Nethersand",
 	tiles = {"nether_sand.png"},
 	is_ground_content = true,
-	groups = {crumbly=3,level=2,falling_node=1},
+	groups = {crumbly = 3, level = 2, falling_node = 1},
 	sounds = default.node_sound_dirt_defaults({
-		footstep = {name="default_gravel_footstep", gain=0.45},
+		footstep = {name = "default_gravel_footstep", gain = 0.45},
 	}),
 })
 
@@ -364,26 +375,20 @@ minetest.register_node("nether:glowstone", {
 	tiles = {"nether_glowstone.png"},
 	is_ground_content = true,
 	light_source = 13,
-	groups = {cracky=3,oddly_breakable_by_hand=3},
+	groups = {cracky = 3, oddly_breakable_by_hand = 3},
 	sounds = default.node_sound_glass_defaults(),
 })
 
 minetest.register_node("nether:brick", {
 	description = "Nether Brick",
 	tiles = {"nether_brick.png"},
-	groups = {cracky=2,level=2},
+	groups = {cracky = 2, level = 2},
 	sounds = default.node_sound_stone_defaults(),
 })
 
-stairs.register_stair_and_slab("nether_brick", "nether:brick",
-	{cracky=3, oddly_breakable_by_hand=1},
-	{"nether_brick.png"},
-	"nether stair",
-	"nether slab",
-	default.node_sound_stone_defaults())
-
 local fence_texture =
 	"default_fence_overlay.png^nether_brick.png^default_fence_overlay.png^[makealpha:255,126,126"
+
 minetest.register_node("nether:fence_nether_brick", {
 	description = "Nether Brick Fence",
 	drawtype = "fencelike",
@@ -397,9 +402,43 @@ minetest.register_node("nether:fence_nether_brick", {
 		type = "fixed",
 		fixed = {-1/7, -1/2, -1/7, 1/7, 1/2, 1/7},
 	},
-	groups = {cracky=3,level=2},
+	groups = {cracky = 3, level = 2},
 	sounds = default.node_sound_stone_defaults(),
 })
+
+
+-- Register stair and slab
+
+stairs.register_stair_and_slab(
+	"nether_brick",
+	"nether:brick",
+	{cracky = 3, oddly_breakable_by_hand = 1},
+	{"nether_brick.png"},
+	"nether stair",
+	"nether slab",
+	default.node_sound_stone_defaults()
+)
+
+
+-- Craftitems
+
+minetest.register_craftitem(":default:mese_crystal_fragment", {
+	description = "Mese Crystal Fragment",
+	inventory_image = "default_mese_crystal_fragment.png",
+	on_place = function(stack, _, pt)
+		if pt.under and minetest.get_node(pt.under).name == "default:obsidian" then
+			local done = make_portal(pt.under)
+			if done and not minetest.setting_getbool("creative_mode") then
+				stack:take_item()
+			end
+		end
+
+		return stack
+	end,
+})
+
+
+-- Crafting
 
 minetest.register_craft({
 	output = "nether:brick",
