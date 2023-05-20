12bd60f3d9d72ecc8bfb41b99472840405d221a3
Diablosxm <132860646+Diablosxm@users.noreply.github.com>
2023-05-20 01:34:51 -0400

    Update nether-compressor-recipe.lua
    clear craft recipe

--- nether-compressor-recipe.lua
+++ nether-compressor-recipe.lua
@@ -14,6 +14,36 @@ local recipes = {
 	{"nether:brick_compressed 9",      "nether:nether_lump", 12}
 
 }
+-- clear craft recipe
+minetest.clear_craft({
+	recipe = {
+		{"nether:rack", "nether:rack"},
+		{"nether:rack", "nether:rack"},
+	}
+})
+
+minetest.clear_craft({
+	recipe = {
+		{"nether:brick","nether:brick","nether:brick"},
+		{"nether:brick","nether:brick","nether:brick"},
+		{"nether:brick","nether:brick","nether:brick"},
+	}
+})
+
+minetest.clear_craft({
+	recipe = {
+		{"nether:rack_deep", "nether:rack_deep"},
+		{"nether:rack_deep", "nether:rack_deep"}
+	}
+})
+
+minetest.clear_craft({
+	recipe = {
+		{"nether:brick_compressed","nether:brick_compressed","nether:brick_compressed"},
+		{"nether:brick_compressed","nether:brick_compressed","nether:brick_compressed"},
+		{"nether:brick_compressed","nether:brick_compressed","nether:brick_compressed"},
+	}
+})
 
 for _, data in pairs(recipes) do
 	technic.register_compressor_recipe({input = {data[1]}, output = data[2], time = data[3]})
