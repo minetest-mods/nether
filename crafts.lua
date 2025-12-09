9d552a8625eaad45b5074cd5982acd52610e0c97
Aleri Kaisattera <73682764+alerikaisattera@users.noreply.github.com>
2025-12-09 17:06:09 +0600

    Fix indentation

--- crafts.lua
+++ crafts.lua
@@ -70,17 +70,18 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-  output = "nether:nether_block",
-  recipe = {
-    {"nether:nether_ingot", "nether:nether_ingot", "nether:nether_ingot"},
-    {"nether:nether_ingot", "nether:nether_ingot", "nether:nether_ingot"},
-    {"nether:nether_ingot", "nether:nether_ingot", "nether:nether_ingot"},
-  }
+	output = "nether:nether_block",
+	recipe = {
+		{"nether:nether_ingot", "nether:nether_ingot", "nether:nether_ingot"},
+		{"nether:nether_ingot", "nether:nether_ingot", "nether:nether_ingot"},
+		{"nether:nether_ingot", "nether:nether_ingot", "nether:nether_ingot"},
+	}
 })
 
 minetest.register_craft({
-  output = "nether:nether_ingot 9",
-  recipe = {{"nether:nether_block"}}
+	output = "nether:nether_ingot 9",
+	recipe = {{"nether:nether_block"}}
 })
 
 -- See tools.lua for tools related crafting
+
