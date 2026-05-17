0462eac76de60fa4c56285c0f4b0894ca4000565
tenplus1 <tenplus1@users.noreply.github.com>
2026-05-17 16:13:01 +0100

    Fix death bug.
    On newer clients the old get_node_group crashes the game, so it has been changed to the newer get_item_group.

--- init.lua
+++ init.lua
@@ -336,7 +336,7 @@ end -- end of "if nether.NETHER_REALM_ENABLED..."
 -- Play bubbling lava sounds if player killed by lava
 minetest.register_on_dieplayer(
 	function(player, reason)
-		if reason.node ~= nil and minetest.get_node_group(reason.node, "lava") > 0 or reason.node == "nether:lava_crust" then
+		if reason.node ~= nil and minetest.get_item_group(reason.node, "lava") > 0 or reason.node == "nether:lava_crust" then
 			minetest.sound_play(
 				"nether_lava_bubble",
 				-- this sample was encoded at 3x speed to reduce .ogg file size
